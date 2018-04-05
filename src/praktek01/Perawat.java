1ef3b83a8890862b5daf5c659cfc21c0669a03b3
SitiMaiSarah980 <32213515+SitiMaiSarah980@users.noreply.github.com>
2018-04-05 21:01:25 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Perawat.java
+++ src/praktek01/Perawat.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author ASUS
  */
 public class Perawat {
+    String NIP;
+    String nama;
+    String tahunkerja;
+    String tanggallahir;
+    String alamat;
     
+    void cetakInfo(){
+        System.out.println("NIP           :"+NIP);
+        System.out.println("Nama          :"+nama);
+        System.out.println("Tahun Bekerja :"+tahunkerja);
+        System.out.println("Tanggal Lahir :"+tanggallahir);
+        System.out.println("Alamat        :"+alamat);
+    }
 }
