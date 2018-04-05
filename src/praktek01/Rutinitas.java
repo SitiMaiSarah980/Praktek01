45b9c493f45594271109521909f3b6ad9423e843
SitiMaiSarah980 <32213515+SitiMaiSarah980@users.noreply.github.com>
2018-04-05 21:12:10 +0800

    Mengisi Nilai Atribut

--- src/praktek01/Rutinitas.java
+++ src/praktek01/Rutinitas.java
@@ -14,5 +14,23 @@ public class Rutinitas {
         Perawat Maisarah = new Perawat();
         Perawat Syarani = new Perawat();
         Perawat Fajar = new Perawat();
+        
+        Maisarah.NIP="001";
+        Maisarah.nama="Siti Mai Sarah";
+        Maisarah.tahunkerja="2005";
+        Maisarah.tanggallahir="5 Mei 1995";
+        Maisarah.alamat="Astambul";
+        
+        Syarani.NIP="010";
+        Syarani.nama="Muhammad Syarani";
+        Syarani.tahunkerja="2001";
+        Syarani.tanggallahir="14 November 1993";
+        Syarani.alamat="Banjarbaru";
+        
+        Fajar.NIP="101";
+        Fajar.nama="Ahmad Fajar Rizqi";
+        Fajar.tahunkerja="2018";
+        Fajar.tanggallahir="8 September 1999";
+        Fajar.alamat="Martapura";
     }
 }
